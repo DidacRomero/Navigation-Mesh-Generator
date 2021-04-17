036fe80c5a4813a5441eca422dbd1e422e7a2f05
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2021-04-17 20:10:43 +0200

    Painting lines from vertex to vertex

--- Navigation-Mesh-Generator/Assets/Resources/Scripts/IdentifyColliderVertices.cs
+++ Navigation-Mesh-Generator/Assets/Resources/Scripts/IdentifyColliderVertices.cs
@@ -28,8 +28,21 @@ public class IdentifyColliderVertices : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
-        foreach (Vector2 pos in verts)
-        Gizmos.DrawWireSphere(pos, 0.14f);
+        
+        //foreach (Vector2 pos in verts)
+        //{
+        //    Gizmos.DrawWireSphere(pos, 0.14f);
+        //}
+        for (int i = 0; i < verts.Count; ++i)
+        {
+            Gizmos.DrawWireSphere(verts[i], 0.14f);
+
+            //Avoid getting out of range on the last vertex
+            if(i< verts.Count -1)
+            Debug.DrawLine(verts[i],verts[i+1], Color.white,0.01f);
+        }
+
+
     }
 
     // Update is called once per frame
